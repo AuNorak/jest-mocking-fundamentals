23497fa52558b74046d1ec2b40dfc41c3d63f51f
Jon Fister <jonfister@gmail.com>
2020-03-22 16:30:58 +1100

    Refactor jest mock function, write frameworkless mock function

--- src/__tests__/mock-fn.js
+++ src/__tests__/mock-fn.js
@@ -9,8 +9,10 @@ test('returns winner', () => {
 
   const winner = thumbWar(player1, player2)
   expect(winner).toBe(player1)
-  expect(utils.getWinner).toHaveBeenCalledTimes(2)
-  expect(utils.getWinner).toHaveBeenCalledWith(player1, player2)
+  expect(utils.getWinner.mock.calls).toEqual([
+    [player1, player2],
+    [player1, player2]
+  ])
 
   utils.getWinner = originalGetWinner
 })
--- src/no-framework/mock-fn.js
+++ src/no-framework/mock-fn.js
@@ -1,3 +1,25 @@
 const assert = require('assert')
 const thumbWar = require('../thumb-war')
 const utils = require('../utils')
+
+function fn(impl) {
+  const mockFn = (...args) => {
+    mockFn.mock.calls.push(args)
+    return impl(...args)
+  }
+  mockFn.mock = {calls: []}
+  return mockFn
+}
+
+const originalGetWinner = utils.getWinner
+const getWinnerStub = (player1, player2) => player1
+utils.getWinner = fn(getWinnerStub)
+
+const winner = thumbWar('Jon', 'Marc')
+assert.strictEqual(winner, 'Jon')
+assert.deepStrictEqual(utils.getWinner.mock.calls, [
+  ['Jon', 'Marc'],
+  ['Jon', 'Marc']
+])
+
+utils.getWinner = originalGetWinner
